bd382337b68bcac76d07ff9675d0f901c66933f7
schBenedikt <benedikt.schaechner@web.de>
2024-10-27 20:46:10 +0100

    Change to Postgres

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for
 import psycopg2
-from psycopg2 import Error
+from psycopg2 import sql, Error
 import time
 
 app = Flask(__name__)
@@ -9,21 +9,24 @@ app = Flask(__name__)
 like_times = {}
 
 def get_db_connection():
-    connection = psycopg2.connect(
-        host='localhost',
-        database='browser_engine',
-        user='your_username',
-        password='your_password'
-    )
-    return connection
+    try:
+        connection = psycopg2.connect(
+            host='localhost',
+            database='search_engine',
+            user='postgres',
+            password='admin'
+        )
+        return connection
+    except Error as e:
+        print(f'Error: {e}')
+        return None
 
 @app.route('/', methods=['GET', 'POST'])
 def search():
     results = []
-    query_time = 0
     message = None
     total_results = 0
-    per_page = 10  # Number of results per page
+    per_page = 10  # Anzahl der Ergebnisse pro Seite
     page = request.args.get('page', 1, type=int)
     query = ""
 
@@ -35,62 +38,51 @@ def search():
 
     start_time = time.time()
     try:
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        if query == "#all":
-            cursor.execute("SELECT COUNT(*) FROM meta_data")
-            total_results = cursor.fetchone()[0]
-            cursor.execute("""
-                SELECT url, title, description, likes, image
-                FROM meta_data
-                ORDER BY likes DESC
-                LIMIT %s OFFSET %s
-            """, (per_page, (page - 1) * per_page))
-        elif query:
-            search_query = ' & '.join(query.split())
-            cursor.execute("""
-                SELECT COUNT(*) FROM meta_data
-                WHERE to_tsvector(title || ' ' || url) @@ to_tsquery(%s)
-            """, (search_query,))
-            total_results = cursor.fetchone()[0]
-
-            if total_results == 0:
-                total_results = 0
-                all_results = []
-                for word in query.split():
-                    word_pattern = f"%{word}%"
-                    cursor.execute("""
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                if query == "#all":
+                    cursor.execute(sql.SQL("""
                         SELECT url, title, description, likes, image
                         FROM meta_data
-                        WHERE title LIKE %s OR url LIKE %s
+                        WHERE title ILIKE %s OR url ILIKE %s
                         ORDER BY likes DESC
                         LIMIT %s OFFSET %s
-                    """, (word_pattern, word_pattern, per_page, (page - 1) * per_page))
+                    """), (search_query, search_query, per_page, (page - 1) * per_page))
                     results = cursor.fetchall()
-                    all_results.extend(results)
-                results = all_results
-                total_results = len(results)
-            else:
-                cursor.execute("""
-                    SELECT url, title, description, likes, image
-                    FROM meta_data
-                    WHERE to_tsvector(title || ' ' || url) @@ to_tsquery(%s)
-                    ORDER BY likes DESC
-                    LIMIT %s OFFSET %s
-                """, (search_query, per_page, (page - 1) * per_page))
-                results = cursor.fetchall()
-        else:
-            cursor.execute("""
-                SELECT url, title, description, image
-                FROM meta_data
-                ORDER BY random()
-                LIMIT 10
-            """)
-            results = cursor.fetchall()
-        cursor.close()
-        connection.close()
+
+                elif query:
+                    # Verwende LIKE zur Suche nach Titel oder URL
+                    search_query = f"%{query}%"
+                    cursor.execute(sql.SQL("""
+                        SELECT COUNT(*) FROM meta_data
+                        WHERE title ILIKE %s OR url ILIKE %s
+                    """), (search_query, search_query))
+                    total_results = cursor.fetchone()[0]
+
+                    if total_results > 0:
+                        cursor.execute(sql.SQL("""
+                            SELECT url, title, description, likes, image
+                            FROM meta_data
+                            WHERE title ILIKE %s OR url ILIKE %s
+                            ORDER BY likes DESC
+                            LIMIT %s OFFSET %s
+                        """), (search_query, search_query, per_page, (page - 1) * per_page))
+                        results = cursor.fetchall()
+                    else:
+                        message = "No results found."
+                else:
+                    # Fetch 10 random crawled pages when no search term is entered
+                    cursor.execute(sql.SQL("""
+                        SELECT url, title, description, image
+                        FROM meta_data
+                        ORDER BY RANDOM()
+                        LIMIT 10
+                    """))
+                    results = cursor.fetchall()
+
     except Error as e:
         print(f'Error: {e}')
+
     query_time = time.time() - start_time
     return render_template('search.html', results=results, query_time=query_time, message=message, total_results=total_results, page=page, per_page=per_page, query=query)
 
@@ -100,18 +92,16 @@ def suggest():
         data = request.get_json()
         query = data['query'].strip()
         
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        cursor.execute("""
-            SELECT url, title
-            FROM meta_data
-            WHERE title LIKE %s
-            LIMIT 5
-        """, ('%' + query + '%',))
-        
-        suggestions = cursor.fetchall()
-        cursor.close()
-        connection.close()
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(sql.SQL("""
+                    SELECT url, title
+                    FROM meta_data
+                    WHERE title ILIKE %s
+                    LIMIT 5
+                """), ('%' + query + '%',))
+                
+                suggestions = cursor.fetchall()
         
         return jsonify({'suggestions': suggestions})
     
@@ -121,28 +111,30 @@ def suggest():
 
 @app.route('/like', methods=['POST'])
 def like():
-    data = request.json
-    url = data.get('url')
-    current_time = time.time()
-    if url:
-        try:
-            if url in like_times and current_time - like_times[url] < 60:
-                return jsonify({'success': False, 'message': 'You can only like once per minute.'}), 400
-            like_times[url] = current_time
-            connection = get_db_connection()
-            cursor = connection.cursor()
-            cursor.execute("""
-                UPDATE meta_data
-                SET likes = likes + 1
-                WHERE url = %s
-            """, (url,))
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return jsonify({'success': True, 'message': 'Liked successfully'}), 200
-        except Error as e:
-            print(f'Error: {e}')
-            return jsonify({'success': False, 'message': 'Error liking the link'}), 500
+    if request.method == 'POST':
+        data = request.json
+        url = data.get('url')
+        current_time = time.time()
+        if url:
+            try:
+                # Überprüfen, ob ein Like für diese URL innerhalb der letzten Minute vergeben wurde
+                if url in like_times and current_time - like_times[url] < 60:
+                    return jsonify({'success': False, 'message': 'You can only like once per minute.'}), 400
+                like_times[url] = current_time  # Speichern der aktuellen Zeit für die URL
+                
+                with get_db_connection() as connection:
+                    with connection.cursor() as cursor:
+                        cursor.execute(sql.SQL("""
+                            UPDATE meta_data
+                            SET likes = likes + 1
+                            WHERE url = %s
+                        """), (url,))
+                        connection.commit()
+                
+                return jsonify({'success': True, 'message': 'Liked successfully'}), 200
+            except Error as e:
+                print(f'Error: {e}')
+                return jsonify({'success': False, 'message': 'Error liking the link'}), 500
     return jsonify({'success': False, 'message': 'Invalid request'}), 400
 
 @app.route('/autocomplete', methods=['GET'])
@@ -150,18 +142,16 @@ def autocomplete():
     term = request.args.get('term')
     if term:
         try:
-            connection = get_db_connection()
-            cursor = connection.cursor()
-            cursor.execute("""
-                SELECT DISTINCT title
-                FROM meta_data
-                WHERE title LIKE %s
-                LIMIT 10
-            """, (f"%{term}%",))
-            results = [row[0] for row in cursor.fetchall()]
-            cursor.close()
-            connection.close()
-            return jsonify(results)
+            with get_db_connection() as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(sql.SQL("""
+                        SELECT DISTINCT title
+                        FROM meta_data
+                        WHERE title ILIKE %s
+                        LIMIT 10
+                    """), (f"%{term}%",))
+                    results = [row[0] for row in cursor.fetchall()]
+                    return jsonify(results)
         except Error as e:
             print(f'Error: {e}')
     return jsonify([])
@@ -171,26 +161,22 @@ def check_single_result():
     term = request.args.get('term')
     if term:
         try:
-            connection = get_db_connection()
-            cursor = connection.cursor()
-            cursor.execute("""
-                SELECT COUNT(*) FROM meta_data
-                WHERE title = %s
-            """, (term,))
-            count = cursor.fetchone()[0]
-            if count == 1:
-                cursor.execute("""
-                    SELECT url FROM meta_data
-                    WHERE title = %s
-                """, (term,))
-                single_result_url = cursor.fetchone()[0]
-                cursor.close()
-                connection.close()
-                return jsonify({'has_single_result': True, 'single_result_url': single_result_url})
-            else:
-                cursor.close()
-                connection.close()
-                return jsonify({'has_single_result': False})
+            with get_db_connection() as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(sql.SQL("""
+                        SELECT COUNT(*) FROM meta_data
+                        WHERE title = %s
+                    """), (term,))
+                    count = cursor.fetchone()[0]
+                    if count == 1:
+                        cursor.execute(sql.SQL("""
+                            SELECT url FROM meta_data
+                            WHERE title = %s
+                        """), (term,))
+                        single_result_url = cursor.fetchone()[0]
+                        return jsonify({'has_single_result': True, 'single_result_url': single_result_url})
+                    else:
+                        return jsonify({'has_single_result': False})
         except Error as e:
             print(f'Error: {e}')
     return jsonify({'has_single_result': False})
